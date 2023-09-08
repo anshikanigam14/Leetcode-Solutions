d83d1955c037f9436fdcd4f27b448c813f410825
Anshika Nigam <63018496+anshikanigam14@users.noreply.github.com>
2023-09-09 00:40:20 +0530

    Time: 45 ms (25.96%), Space: 16.3 MB (31.34%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,30 @@
+import re
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        pattern = r'[0-9]'
+        word_dict = {}
+        output = ""
+        s_split = s.split()
+        for word in s_split:
+            for i in range(0, len(word)):
+                if word[i].isnumeric():
+                    word_new = re.sub(pattern,'', word)
+                    word_dict.update({int(word[i]):word_new})
+        word_dict = dict(sorted(word_dict.items()))
+        # print(word_dict)
+        for key, value in word_dict.items():
+            output = output + ' ' + value
+            output = output.strip()
+
+        return output
+
+
+
+
+
+
+
+
+
+
+        
